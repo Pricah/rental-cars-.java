15cf46e74e442b0ccbe29fc79995737566bf8c8e
Pricah <pricalyf@gmail.com>
2025-02-06 22:34:42 +0300

    Update agency.java

---
+++ agency.java
@@ -0,0 +1,30 @@
+package services;
+
+import models.Car;
+import models.Customer;
+import models.RentalTransaction;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentalAgency {
+    private List<Car> cars = new ArrayList<>();
+    private List<Customer> customers = new ArrayList<>();
+    private List<RentalTransaction> transactions = new ArrayList<>();
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void addCustomer(Customer customer) {
+customers.add(customer);
+    }
+
+    public RentalTransaction rentCar(String carId, String customerId, int days) {
+        Car car = cars.stream().filter(c -> c.getId().equals(carId) && c.isAvailable()).findFirst().orElse(null);
+        Customer customer = customers.stream().filter(c -> c.getId().equals(customerId)).findFirst().orElse(null);
+
+        if (car!= null && customer!= null) {
+RentalTransaction transaction = new RentalTransaction(car, customer, days);
+            transactions.add(transaction);
+            return transaction;
+        }
